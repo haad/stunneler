174280e474682e5655ad02b2cc65273e5b5b0f27
Adam Hamsik <adam.hamsik@chilisys.com>
2013-01-26 21:22:14 +0100

    Add first version with libssh.

--- Makefile
+++ Makefile
@@ -1,15 +1,26 @@
 
 CC=gcc
 
-CWARN=-std=c99 -pedantic -Wall -Wshadow -Wpointer-arith -Wcast-qual \
+ifdef RELEASE
+COPTS=-std=c99 -pedantic -Wall -Wshadow -Wpointer-arith -Wcast-qual \
         -Wstrict-prototypes -Wmissing-prototypes
-COPTS=-O2
-CDEB=-g
+COPTS+=-O2
+else
+COPTS=-std=c99 -pedantic -Wall \
+    -Wextra  -Wformat=2 -Wswitch-default -Wswitch-enum -Wcast-align -Wpointer-arith \
+    -Wbad-function-cast -Wstrict-overflow=5 -Wstrict-prototypes -Winline \
+    -Wundef -Wnested-externs -Wcast-qual -Wshadow  \
+    -Wfloat-equal -Wstrict-aliasing=2 -Wredundant-decls \
+    -Wold-style-definition -Werror
 
-CFLAGS=-I dist/cjson $(COPTS) $(CDEB) $(CWARN)
+COPTS+=-g -O0 -fno-omit-frame-pointer  -fno-common -fstrict-aliasing
+endif
+
+CFLAGS=-I dist/cjson $(COPTS)
+LDFLAGS=  -lssh -lcurl
 
 stunneler: src/stunneler.o dist/cjson/cJSON.o
-	$(CC) -o stunneler src/stunneler.o dist/cjson/cJSON.o
+	$(CC) -o stunneler src/stunneler.o dist/cjson/cJSON.o $(LDFLAGS)
 
 clean:
 	rm -rf src/*.o dist/cjson/*.o
--- src/stunneler.c
+++ src/stunneler.c
@@ -1,5 +1,8 @@
 #include <sys/types.h>
+#include <sys/param.h>
 #include <sys/stat.h>
+
+#include <libssh/libssh.h>
 
 #include <errno.h>
 #include <fcntl.h>
@@ -12,33 +15,136 @@
 
 #define STUNEL_CONFIG "stuneler.conf"
 
-size_t get_conf_file_size(int);
+static size_t get_conf_file_size(char *path);
+static void usage(void);
+static cJSON * get_conf_file(char *);
+static int rem_ssh_connect(cJSON *);
 
-size_t
-get_conf_file_size(int conf_fd)
+cJSON *conf_json;
+
+static void
+usage(void)
 {
+	printf("stunneler command to persitently call home and open tunnels to local port 22.\n");
+	exit(EXIT_SUCCESS);
+}
+
+static size_t
+get_conf_file_size(char *path)
+{
+	int fd;
 	struct stat sb;
 
-	if ( fstat(conf_fd, &sb) != 0 ) {
+	if ((fd = open(path, O_RDONLY)) < 0) {
+		fprintf(stderr, "Cannot open config file from %s.\n", path);
+		exit(EXIT_FAILURE);
+	}
+
+	if ( fstat(fd, &sb) != 0 ) {
 		fprintf(stderr, "Could not fstat a opened file.\n");
 		exit(EXIT_FAILURE);
 	}
 
+	close(fd);
+
 	return (size_t)sb.st_size;
 }
 
-
-int main(int argc, char **argv)
+static cJSON *
+get_conf_file(char *conf_path)
 {
-	int conf_fd;
+	FILE *conf_f;
+	cJSON *json;
 
-	if ((conf_fd = open(STUNEL_CONFIG, O_RDONLY)) < 0) {
+	char *mmap_file;
+	size_t conf_size;
+
+	printf("Opening config file at %s.\n", conf_path);
+	conf_size = get_conf_file_size(conf_path);
+
+	if ((mmap_file = malloc(conf_size * sizeof(char))) == NULL) {
+		fprintf(stderr, "Cannot allocate memory for conf_file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((conf_f = fopen(conf_path, "r")) == NULL) {
 		fprintf(stderr, "Cannot open config file from %s.\n", STUNEL_CONFIG);
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Opening config file at %s.\n", STUNEL_CONFIG);
+	fread(mmap_file, 1, conf_size, conf_f);
 
-	close(conf_fd);
+	json = cJSON_Parse(mmap_file);
+
+	free(mmap_file);
+	fclose(conf_f);
+
+	return json;
+}
+
+static int
+rem_ssh_connect(cJSON *json)
+{
+	ssh_session my_ssh_session;
+	int verbosity = SSH_LOG_PROTOCOL;
+	int port = 22, rc;
+
+	json = NULL;
+
+	if ((my_ssh_session  = ssh_new()) == NULL) {
+		fprintf(stderr, "Creating new ssh_session failed.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	ssh_options_set(my_ssh_session, SSH_OPTIONS_HOST, "localhost");
+	ssh_options_set(my_ssh_session, SSH_OPTIONS_LOG_VERBOSITY, &verbosity);
+	ssh_options_set(my_ssh_session, SSH_OPTIONS_PORT, &port);
+
+	rc = ssh_connect(my_ssh_session);
+  	if (rc != SSH_OK)
+	{
+		fprintf(stderr, "Error connecting to localhost: %s\n",
+	            ssh_get_error(my_ssh_session));
+		exit(EXIT_FAILURE);
+	}
+
+	ssh_disconnect(my_ssh_session);
+	ssh_free(my_ssh_session);
+
+	return 0;
+}
+
+int
+main(int argc, char **argv)
+{
+	int ch;
+	char conf_path[MAXPATHLEN];
+
+	(void)strncpy(conf_path, STUNEL_CONFIG, MAXPATHLEN-1);
+
+	/* Parse command line args */
+	while ((ch = getopt(argc, argv, "hf:")) != -1) {
+		switch (ch) {
+			case 'f':
+				memset(conf_path, 0, MAXPATHLEN);
+				(void)strncpy(conf_path, optarg, MAXPATHLEN-1);
+				conf_path[MAXPATHLEN - 1]='\0';
+				break;
+			case '?':
+			case 'h':
+			default:
+				usage();
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	conf_json = get_conf_file(conf_path);
+
+	printf("Config file: \n%s\n", cJSON_Print(conf_json));
+
+	printf("Connecting with libssh\n");
+	rem_ssh_connect(conf_json);
+
 	return 0;
 }
